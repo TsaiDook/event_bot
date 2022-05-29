50ddfa0a6b06023a61efecaf83fa79668f98cd8d
Alexey Bezgin <67886861+elderberry17@users.noreply.github.com>
2022-05-29 20:20:22 +0300

    not bad bod body

--- bot_body.py
+++ bot_body.py
@@ -4,6 +4,7 @@ import config
 from users_tb_action import insert_user, check_existence, update_user_tb, update_user_hobbies_tb, update_user_topics_tb, \
     get_user_tb_column_val
 from match_users import interests_match
+from events_tb_action import insert_event, update_event_tb
 import re
 import datetime
 
@@ -93,39 +94,51 @@ def update_user_data(call):
     if call.message:
         answer = call.data
         username = call.message.chat.username
-        stage = get_user_tb_column_val(username, "info_stage")
+        info_stage = get_user_tb_column_val(username, "info_stage")
+        event_stage = get_user_tb_column_val(username, "event_stage")
         chat_id = call.message.chat.id
-        if answer in config.genders and stage == 0:
+        now = datetime.datetime.now()
+        day_options = [(now + datetime.timedelta(days=i)).strftime("%d.%m.%y") for i in range(8)]
+        if answer in config.genders and info_stage == 0:
             update_user_tb(username, "gender", answer)
             bot.send_message(chat_id, text=f'Gender has been changed to "{answer}"')
             get_age(chat_id)
             update_user_tb(username, "info_stage", 1)
-        elif answer in config.ages and stage == 1:
+        elif answer in config.ages and info_stage == 1:
             update_user_tb(username, "age", answer)
             bot.send_message(chat_id, text=f'Age has been changed to "{answer}"')
             get_hobbies(chat_id)
             update_user_tb(username, "info_stage", 2)
-        elif answer in config.common_hobbies and answer != 'DONE' and stage in [2, 3]:
+        elif answer in config.common_hobbies and answer != 'DONE' and info_stage in [2, 3]:
             update_user_hobbies_tb(username, config.hobbies_to_eng[answer])
             bot.send_message(chat_id, text=f'Hobby "{answer}" has been added')
             update_user_tb(username, "info_stage", 3)
-        elif answer == 'DONE' and stage == 3:
+        elif answer == 'DONE' and info_stage == 3:
             get_conv_topics(chat_id)
             update_user_tb(username, "info_stage", 4)
-        elif answer in config.common_conv_topics and answer != 'FINISH' and stage in [4, 5]:
+        elif answer in config.common_conv_topics and answer != 'FINISH' and info_stage in [4, 5]:
             update_user_topics_tb(username, config.topics_to_eng[answer])
             bot.send_message(chat_id, text=f'Topic "{answer}" has been added')
             update_user_tb(username, "info_stage", 5)
-        elif answer == 'FINISH' and stage == 5:
+        elif answer == 'FINISH' and info_stage == 5:
             bot.send_message(chat_id,
                              text='Awesome!\nEventually add a little self-description:')
             update_user_tb(username, "info_stage", 6)
+        elif answer in day_options and event_stage == 0:
+            update_event_tb(username, "day", answer)
+            update_user_tb(username, "event_stage", 1)
+            bot.send_message(chat_id, text=f'Great! Day is {answer}. Now enter time:')
+            get_time_period(chat_id)
+        elif answer in config.time_periods and event_stage == 1:
+            update_event_tb(username, "time", answer)
+            bot.send_message(chat_id, text=f'Ok, time is "{answer}"\nMake a little event description:')
+            update_user_tb(username, "event_stage", 2)
 
 
 def get_date(chat_id):
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     now = datetime.datetime.now()
-    options = [(now + datetime.timedelta(days=i)).strftime("%d:%m:%y") for i in range(8)]
+    options = [(now + datetime.timedelta(days=i)).strftime("%d.%m.%y") for i in range(8)]
     buttons = [types.InlineKeyboardButton(text=option,
                                           callback_data=option)
                for option in options]
@@ -148,20 +161,12 @@ def get_time_period(chat_id):
                      reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def update_event_data(call):
-    if call.message:
-        answer = call.data
-        username = call.message.chat.username
-        event_stage = get_user_tb_column_val(username, "event_stage")
-        chat_id = call.message.chat.id
-
-
 @bot.message_handler(content_types=['text'])
 def communicate(message):
     message_text = message.text
     username = message.from_user.username
     info_stage = get_user_tb_column_val(username, "info_stage")
+    event_stage = get_user_tb_column_val(username, "event_stage")
     chat_id = message.chat.id
     if message_text == "Find event":
         if info_stage == 7:
@@ -170,7 +175,10 @@ def communicate(message):
             bot.send_message(chat_id, text="You have to fill information about yourself for start!")
     elif message_text == "Create event":
         if info_stage == 7:
-            bot.send_message(chat_id, text="Describe the event you wanna add!")
+            update_user_tb(username, "event_stage", 0)
+            insert_event(username)
+            bot.send_message(chat_id, text="Describe the event you wanna add!\nChoose the date:")
+            get_date(chat_id)
         else:
             bot.send_message(chat_id, text="You have to fill information about yourself for start!")
     elif message_text == "Find similar users":
@@ -185,6 +193,11 @@ def communicate(message):
         get_gender(chat_id)
     elif message_text == "Back to main menu":
         back_to_main_menu(chat_id)
+    elif event_stage == 2:
+        update_event_tb(username, "description", message_text)
+        bot.send_message(chat_id,
+                         text=f'Event description:\n"{message_text}"\nYou may change it later')
+        update_user_tb(username, "event_stage", 3)
     # now we are getting a self-description. otherwise we shall ignore a user
     elif info_stage == 6:
         update_user_tb(username, "self_description", message_text)
