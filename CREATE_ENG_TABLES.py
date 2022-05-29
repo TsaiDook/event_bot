4ff8fc0cf9c1dbde6bbeb44a2c3171dad3e6d045
Alexey Bezgin <67886861+elderberry17@users.noreply.github.com>
2022-05-29 13:00:02 +0300

    CREATE NEW TABLES USING IT

---
+++ CREATE_ENG_TABLES.py
@@ -0,0 +1,29 @@
+HOBBIES = """CREATE TABLE user_hobbies(
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+Table_games BOOL,
+Education BOOL,
+Art BOOL,
+Science BOOL,
+Pets BOOL,
+Busyness BOOL,
+Improving BOOL,
+Sport BOOL,
+Blogging BOOL,
+Computer_games BOOL)"""
+
+TOPICS = """CREATE TABLE user_topics(
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+Thoughts BOOL,
+Education BOOL,
+Art BOOL,
+Experiences BOOL,
+Busyness BOOL,
+Work BOOL,
+Sport BOOL,
+Anxiety BOOL,
+Travels BOOL,
+Politics BOOL,
+Humour BOOL,
+Future BOOL)"""
