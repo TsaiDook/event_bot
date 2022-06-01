f3aa317fd7bbb7370ebd9572d7fe1b1b3343f874
Arthur <izmodizm@gmail.com>
2022-06-01 17:43:09 +0300

    almost mvp, need to add users class

--- bot_body.py
+++ bot_body.py
@@ -3,15 +3,15 @@ from telebot import types
 import config
 from users_tb_action import insert_user, check_existence, update_user_tb, update_user_hobbies_tb, update_user_topics_tb, \
     get_user_tb_column_val, get_interest_val, get_interest
-from match_users import interests_match, extract_features
+from match_users import interests_match
 from events_tb_action import insert_event, update_event_tb, get_all_events_by_day, delete_event, get_event_by_creator
 from match_events import event_match
 import datetime
 
 bot = telebot.TeleBot(config.token)
 
-
 # class User():
+users = {}
 
 
 @bot.message_handler(commands=['start'])
@@ -25,12 +25,13 @@ def start_msg(message):
                      reply_markup=markup)
     if not check_existence(username):
         insert_user(username=username)
+    users[username] = {"match_users_row": 0, "match_events_row": 0}
     main_menu(message)
 
 
 def main_menu(message):
     key = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    key.row("Мой профиль", "События", "Найти похожих юзеров")
+    key.row("Мой профиль", "События", "Найти похожих пользователей")
     send = bot.send_message(message.from_user.id, "Ты в главном меню", reply_markup=key)
     bot.register_next_step_handler(send, profile_events_match_btns)
 
@@ -52,12 +53,60 @@ def profile_events_match_btns(message):
         send = bot.send_message(message.from_user.id, "Что хочешь сделать?", reply_markup=keyboard)
         bot.register_next_step_handler(send, event_btns)
 
-    elif message.text == "Найти похожих юзеров":
+    elif message.text == "Найти похожих пользователей":
         if get_user_tb_column_val(message.from_user.username, "info_stage") == 6:
-            # TODO find users
-            pass
+            make_users_match(message.from_user.username, message.chat.id)
+            keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            keyboard.row("Показать еще пользователей", "Вернуться в меню")
+            send = bot.send_message(message.chat.id, text="Лови чуваков:", reply_markup=keyboard)
+            bot.register_next_step_handler(send, match_handler)
         else:
             bot.send_message(message.chat.id, text="Сначала введи данные")
+
+
+def make_users_match(username, chat_id, start=config.start, top_n=config.top_n):
+    challengers = interests_match(username)
+    try:
+        challengers = challengers[start:start + top_n]
+    except IndexError:
+        try:
+            challengers = challengers[start:]
+        except IndexError:
+            challengers = None
+    if challengers:
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+        keyboard.row("Показать еще пользователей", "В главное меню")
+        bot.send_message(chat_id, text="По-моему, эти чуваки могли бы составить тебе компанию:", reply_markup=keyboard)
+        for user in challengers:
+            bot.send_message(chat_id, text=get_user_profile_text(user[1]))
+    else:
+        bot.send_message(chat_id, text="Никого не нашел :(")
+
+
+def match_handler(message):
+    if message.text == "Показать еще пользователей":
+        users[message.from_user.username]["match_users_row"] += 1
+        attempt = users[message.from_user.username]["match_users_row"]
+        make_users_match(message.from_user.username, message.chat.id, start=config.start + attempt * config.top_n)
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+        keyboard.row("Показать еще пользователей", "Вернуться в меню")
+        send = bot.send_message(message.chat.id, text="Чекай чуваков", reply_markup=keyboard)
+        bot.register_next_step_handler(send, match_handler)
+    elif message.text == "Показать еще события":
+        users[message.from_user.username]["match_events_row"] += 1
+        attempt = users[message.from_user.username]["match_events_row"]
+        make_event_match(message.from_user.username, message.chat.id,
+                         event_day=users[message.from_user.username]["event_day"],
+                         event_time=users[message.from_user.username]["event_time"],
+                         start=config.start + attempt * config.top_n)
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+        keyboard.row("Показать еще события", "Вернуться в меню")
+        send = bot.send_message(message.chat.id, text="Чекай чуваков", reply_markup=keyboard)
+        bot.register_next_step_handler(send, match_handler)
+    elif message.text == "Вернуться в меню":
+        users[message.from_user.username]["match_users_row"] = 0
+        users[message.from_user.username]["match_events_row"] = 0
+        main_menu(message)
 
 
 def event_btns(message):
@@ -67,10 +116,8 @@ def event_btns(message):
         update_user_tb(message.from_user.username, "event_stage", 0)
         get_date(message.chat.id)
     elif message.text == "Найти события":
-        # TODO find events
-        pass
-    elif message.text == "Вернуться в меню":
-        main_menu(message)
+        update_user_tb(message.from_user.username, "searching_stage", 1)
+        get_date(message.chat.id)
 
 
 def get_date(chat_id):
@@ -130,6 +177,7 @@ def edit_profile(message):
     elif message.text == "Вернуться в меню":
         main_menu(message)
 
+
 def get_gender(chat_id):
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     buttons = [types.InlineKeyboardButton(text=gender,
@@ -175,6 +223,7 @@ def get_conv_topics(chat_id):
                      'С хобби закончили!\nА о чем ты предпочитаешь поговорить? ***Выбери от 1 до 8 вариантов:***',
                      reply_markup=keyboard, parse_mode="Markdown")
 
+
 def self_describing(message):
     update_user_tb(message.from_user.username, "self_description", message.text)
     main_menu(message)
@@ -187,36 +236,30 @@ def event_describing(message):
     sent = bot.send_message(message.from_user.id, "Готово! Что дальше?", reply_markup=keyboard)
     bot.register_next_step_handler(sent, event_btns)
 
-# It is not logical that this piece of code is here, not in match_users.py
-# def make_user_match(username, chat_id):
-#     challengers = interests_match(username)
-#     if challengers:
-#         bot.send_message(chat_id, text="По-моему, эти чуваки могли бы составить тебе компанию:")
-#         for challenger in challengers:
-#             challenger_hobbies = ', '.join(challenger[1])
-#             challenger_topics = ', '.join(challenger[2])
-#             bot.send_message(chat_id,
-#                              text=f"""***Хобби:***\n{challenger_hobbies}\n\n***Любимые темы для разговора:***\n{challenger_topics}\n\n***О себе:***\n"{challenger[3]}"\n\n***Тэг в Телеграмме:***\n@{challenger[0]}""",
-#                              parse_mode="Markdown")
-#     else:
-#         bot.send_message(chat_id, text="Пока что я не могу ни с кем тебя помэтчить :(")
-#
-#
-# def make_event_match(username, chat_id, event_day=None, event_time=None):
-#     possible_options = get_all_events_by_day(event_day, username)
-#     if possible_options:
-#         best_options = event_match(username, event_day, event_time)
-#         for option in best_options:
-#             creator_name, suggested_time, event_description = option[:3]
-#             bot.send_message(chat_id,
-#                              text=f"""***Начало:***\n{suggested_time}\n\n***Описание:***\n{event_description}\n\n***Автор:***\n@{creator_name}""",
-#                              parse_mode="Markdown")
-#     else:
-#         bot.send_message(chat_id, text="В этот день пока что никто не тусит, но ты можешь создать свое событие!")
-#
-#     update_user_tb(username, "searching_stage", 0)
-#
-#
+
+def make_event_match(username, chat_id, event_day=None, event_time=None, start=config.start):
+    possible_options = get_all_events_by_day(event_day, username)
+    if possible_options:
+        best_options = event_match(username, event_day, event_time)
+        try:
+            best_options = best_options[start:start + config.top_n]
+        except IndexError:
+            try:
+                best_options = best_options[start:]
+            except IndexError:
+                best_options = None
+
+        if best_options:
+            for option in best_options:
+                creator_name, suggested_time, event_description = option[:3]
+                bot.send_message(chat_id,
+                                 text=f"""***Начало:***\n{suggested_time}\n\n***Описание:***\n{event_description}\n\n***Автор:***\n@{creator_name}""",
+                                 parse_mode="Markdown")
+    else:
+        bot.send_message(chat_id, text="В этот день пока что никто не тусит, но ты можешь создать свое событие!")
+
+    update_user_tb(username, "searching_stage", 0)
+
 
 def reset_info(username):
     update_user_tb(username, "info_stage", 0)
@@ -227,15 +270,12 @@ def reset_info(username):
 
 
 def get_user_profile_text(username):
-    user_hobbies, user_topics = extract_features(username)[:2]
+    user_hobbies = get_interest(username, "hobbies")
+    user_topics = get_interest(username, "topics")
     user_hobbies = ", ".join(user_hobbies) if user_hobbies else "Пусто"
     user_topics = ", ".join(user_topics) if user_topics else "Пусто"
     self_description = get_user_tb_column_val(username, "self_description")
     return f"""<b>Хобби</b>:\n{user_hobbies}\n\n<b>Любимые темы для разговора:</b>\n{user_topics}\n\n<b>О себе:</b>\n"{self_description}"\n\n<b>Тэг в Телеграмме:</b>\n@{username}"""
-
-
-# КОСТЫЛИЩЕ
-users = {}
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -243,8 +283,7 @@ def update_user_data(call):
     if call.message:
         answer = call.data
         username = call.message.chat.username
-        if username not in users:
-            users[username] = {"event_day": None, "event_time": None}
+
         info_stage = get_user_tb_column_val(username, "info_stage")
         event_stage = get_user_tb_column_val(username, "event_stage")
         searching_stage = get_user_tb_column_val(username, "searching_stage")
@@ -294,17 +333,22 @@ def update_user_data(call):
             bot.send_message(chat_id, text="Укаажи хотя бы одну тему")
 
 
-        # elif answer in day_options and searching_stage == 1:
-        #     answer = str(datetime.datetime.strptime(answer, "%d.%m.%y"))[:10].replace('-', '.')
-        #     users[username]["event_day"] = answer
-        #     bot.send_message(chat_id, text=f'День: {answer}\nТеперь выбери время:')
-        #     get_time_period(chat_id)
-        #     update_user_tb(username, "searching_stage", 2)
+        elif answer in day_options and searching_stage == 1:
+            answer = str(datetime.datetime.strptime(answer, "%d.%m.%y"))[:10].replace('-', '.')
+            users[username]["event_day"] = answer
+            bot.send_message(chat_id, text=f'День: {answer}\nТеперь выбери время:')
+            get_time_period(chat_id)
+            update_user_tb(username, "searching_stage", 2)
 
-        # elif answer in config.time_periods and searching_stage == 2:
-        #     users[username]["event_time"] = answer
-        #     bot.send_message(chat_id, text=f'Принял! Думаю, эти варианты неплохи:')
-        #     make_event_match(username, chat_id, users[username]["event_day"], users[username]["event_time"])
+        elif answer in config.time_periods and searching_stage == 2:
+            users[username]["event_time"] = answer
+            bot.send_message(chat_id, text=f'Принял! Думаю, эти варианты неплохи:')
+            make_event_match(username, chat_id, users[username]["event_day"], users[username]["event_time"])
+
+            keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            keyboard.row("Показать еще события", "Вернуться в меню")
+            sent = bot.send_message(call.message.chat.id, "Готово! Что дальше?", reply_markup=keyboard)
+            bot.register_next_step_handler(sent, match_handler)
 
         elif answer in day_options and event_stage == 0:
             answer = str(datetime.datetime.strptime(answer, "%d.%m.%y"))[:10].replace('-', '.')
@@ -319,89 +363,5 @@ def update_user_data(call):
             update_user_tb(username, "event_stage", 2)
             bot.register_next_step_handler(sent, event_describing)
 
-
-def change_activity_status(username, chat_id):
-    curr_activity = get_user_tb_column_val(username, "is_active")
-    if curr_activity == 1:
-        update_user_tb(username, "is_active", 0)
-        bot.send_message(chat_id,
-                         text="Теперь тебя не потревожат!")
-    else:
-        update_user_tb(username, "is_active", 1)
-        bot.send_message(chat_id,
-                         text="Ты снова виден другим!")
-
-
-#
-# @bot.message_handler(content_types=['text'])
-# def communicate(message):
-#     message_text = message.text
-#     username = message.from_user.username
-#     info_stage = get_user_tb_column_val(username, "info_stage")
-#     event_stage = get_user_tb_column_val(username, "event_stage")
-#     chat_id = message.chat.id
-#
-#     if message_text == "Найти событие":
-#         if info_stage == 7:
-#             bot.send_message(chat_id, text="Опиши мне его!")
-#             update_user_tb(username, "searching_stage", 1)
-#             get_date(chat_id)
-#         else:
-#             bot.send_message(chat_id, text='Для начала расскажи о себе в ***Редактировать профиль***!',
-#                              parse_mode="Markdown")
-#
-#     elif message_text == "Создать событие":
-#         if info_stage == 7:
-#             if event_stage != 3:
-#                 insert_event(username)
-#                 bot.send_message(chat_id, text="Опиши мне его!\nУкажи дату:")
-#                 get_date(chat_id)
-#             else:
-#                 edit_event(username, chat_id)
-#         else:
-#             bot.send_message(chat_id, text='Для начала расскажи о себе в ***Редактировать профиль***!',
-#                              parse_mode="Markdown")
-#
-#
-#     elif message_text == "Найти похожих юзеров":
-#         if info_stage == 7:
-#             make_user_match(username, chat_id)
-#         else:
-#             bot.send_message(chat_id, text='Для начала расскажи о себе в ***Редактировать профиль***!',
-#                              parse_mode="Markdown")
-#
-#     elif message_text == 'Редактировать профиль':
-#         # clear old info
-#         reset_info(username)
-#         # start to getting data from user
-#         bot.send_message(chat_id, "Давай начнем!")
-#         get_gender(chat_id)
-#
-#     # now we are getting a self-description. otherwise we shall ignore a user
-#     elif info_stage == 6 and message_text:
-#         update_user_tb(username, "self_description", message_text)
-#         bot.send_message(chat_id,
-#                          text=f'Твое описание:\n"{message_text}"\n\n***Ты можешь ввести информацию о себе сначала в "Редактировать профиль", если захочешь изменить что-то***',
-#                          parse_mode="Markdown")
-#         update_user_tb(username, "info_stage", 7)
-#
-#     elif message_text == "Сменить видимость":
-#         change_activity_status(username, chat_id)
-#
-#     elif message_text == "Мой профиль":
-#         if info_stage == 7:
-#             show_user_profile(username, chat_id)
-#         else:
-#             bot.send_message(chat_id,
-#                              text='Пока что ты не указал о себе нужную информацию)\nМожешь сделать это в ***"Редактировать профиль"***',
-#                              parse_mode="Markdown")
-#
-#     elif event_stage == 2 and message_text:
-#         update_event_tb(username, "description", message_text)
-#         bot.send_message(chat_id,
-#                          text=f'Описание события:\n"{message_text}"\n\n***Ты можешь изменить или удалить его***',
-#                          parse_mode="Markdown")
-#         update_user_tb(username, "event_stage", 3)
-#
 
 bot.polling(none_stop=True)
--- match_users.py
+++ match_users.py
@@ -1,37 +1,53 @@
-# According to a little experiment, vectorized matching algorithm works better
+# According to a little experiment, summarizing matching algorithm works better
 # So We use it in the final version of the project
-from config import age_to_num
-from users_tb_iter import get_interest, get_user_tb_column_val, get_active_users
-from scipy import spatial
+import config
+from users_tb_action import get_interest, get_user_tb_column_val, get_active_users
 
 
-def extract_features(username):
-    age = age_to_num[get_user_tb_column_val(username, "age")]
-    lovely_hobbies, hobbies_vector = get_interest(username, 'hobbies')
-    lovely_topics, topics_vector = get_interest(username, 'topics')
+def count_feature_similarity(usr1, usr2, feature) -> int:
+    usr1_values = get_interest(usr1, feature)[0]
+    usr2_values = get_interest(usr2, feature)[0]
+    data = config.hobbies_to_eng.values() if feature == "hobbies" else config.topics_to_eng.values()
+    summa = 0
+    for sample in data:
+        if sample in usr1_values and sample in usr2_values:
+            summa += 1
+        elif sample not in usr1_values and not usr2_values:
+            summa += 0.1
+        else:
+            summa -= 0.2
+    return summa
 
-    return lovely_hobbies, lovely_topics, [age, *hobbies_vector, *topics_vector]
+
+def sum_user_similarity(usr1, usr2):
+    same_hobbies = count_feature_similarity(usr1, usr2, 'hobbies')
+    same_topics = count_feature_similarity(usr1, usr2, 'conv_topics')
+    usr1_age = config.age_to_num[get_user_tb_column_val(usr1, 'age')]
+    usr2_age = config.age_to_num[get_user_tb_column_val(usr2, 'age')]
+    # 11 -- max age diff
+    age_diff = abs(usr1_age - usr2_age) / 11
+    return same_topics + same_hobbies - age_diff
 
 
-def cosine_similarity(usr1, usr2):
-    return spatial.distance.cosine(usr1, usr2)
-
-
-# The cold start problem -- ?
-def interests_match(username, top_n=3):
-    user_hobbies, user_topics, user_vector = extract_features(username)
+# make return top n massiv
+def interests_match(user):
     # Structure: username, hobbies, topics, description, similarity
+    other_usernames = get_active_users(user)
     challengers = []
-    other_usernames = get_active_users(username)
-    for name in other_usernames:
-        challenger_hobbies, challenger_topics, challenger_vector = extract_features(name)
-        challenger_description = get_user_tb_column_val(name, "self_description")
-        similarity = cosine_similarity(user_vector, challenger_vector)
-        challengers.append(tuple([name, challenger_hobbies, challenger_topics, challenger_description, similarity]))
-    challengers.sort(key=lambda x: x[4], reverse=True)
-    try:
-        return challengers[:top_n]
-    except IndexError:
-        return challengers
+    for challenger in other_usernames:
+        similarity = sum_user_similarity(user, challenger)
+        # lovely_hobbies = get_interest(challenger, "hobbies")[0]
+        # lovely_hobbies = list(
+        #     map(lambda x: list(config.hobbies_to_eng.keys())[list(config.hobbies_to_eng.values()).index(x)],
+        #         lovely_hobbies))
+        # lovely_topics = get_interest(challenger, "topics")[0]
+        # lovely_topics = list(
+        #     map(lambda x: list(config.topics_to_eng.keys())[list(config.topics_to_eng.values()).index(x)],
+        #         lovely_topics))
+        # description = get_user_tb_column_val(challenger, "self_description")
+        # challenger_age = get_user_tb_column_val(challenger, "age")
 
-# match("user1")
+        challengers.append(tuple([similarity, challenger]))
+
+    challengers.sort(key=lambda x: x[0], reverse=True)
+    return challengers
