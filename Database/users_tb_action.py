5f5a9fd6d9712d6299b641e2a5a9358ca1a9f1e8
Alexey Bezgin <67886861+elderberry17@users.noreply.github.com>
2022-06-02 23:35:33 +0300

    with documentation!!

--- Database/users_tb_action.py
+++ Database/users_tb_action.py
@@ -4,6 +4,11 @@ from ConstantsClass import Constants
 
 
 def create_connection():
+    """
+
+    Create connection to database
+
+    """
     database_params = Constants.database_params
     connection = connect(host=database_params["host"], user=database_params["user"],
                          password=database_params["password"],
@@ -13,6 +18,26 @@ def create_connection():
 
 def insert_user(age=None, gender=None, username=None, self_description="Пусто", curr_info_stage=0, curr_event_stage=0,
                 curr_searching_stage=0, is_active=True):
+    """
+
+    Insert new event in database
+    :param age: user's age group
+    :type: string
+    :param gender: user's gender
+    :type: string
+    :param username: user's telegram username
+    :type: string
+    :param self_description: user's self-description
+    :type: string
+    :param curr_info_stage:
+    :param curr_event_stage: a stage user has filled info about themselves
+    :type: int
+    :param curr_searching_stage: a stage user has filled info about an event they are looking for now
+    :type: int
+    :param is_active: boolean variable shows if user want other people are able to see them
+    :type: bool
+
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -33,6 +58,13 @@ def insert_user(age=None, gender=None, username=None, self_description="Пуст
 
 
 def add_user_hobbies_row(user_id):
+    """
+
+    Insert new user in hobbies table
+    :param user_id: user's telegram id
+    :type: int
+    
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -49,6 +81,13 @@ def add_user_hobbies_row(user_id):
 
 
 def add_user_topics_row(user_id):
+    """
+
+    Insert new user in conversation topics table
+    :param user_id: user's telegram id
+    :type: int
+
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -65,6 +104,18 @@ def add_user_topics_row(user_id):
 
 
 def get_user_tb_column_val(username, column):
+    """
+
+    Return an exact column's value from users table
+    :param username: telegram username of an author
+    :type: string
+    :param column: a column, which value we went to get
+    :type: string
+
+    :rtype: string
+    :return: a value of the mentioned column from users table for an exact user
+
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -81,6 +132,18 @@ def get_user_tb_column_val(username, column):
 
 
 def get_interest(username, interest):
+    """
+
+    Return user's interests: hobbies or conversation topics depends on an 'interest' variable value
+    :param username: telegram username of an author
+    :type: string
+    :param interest: a column, which value we want to get
+    :type: string
+
+    :rtype: list
+    :return: user's hobbies or preferred conversation topics
+
+    """
     user_id = get_user_tb_column_val(username, "id")
     try:
         tb_name = "user_hobbies" if interest == "hobbies" else "user_topics"
@@ -111,6 +174,13 @@ def get_interest(username, interest):
 
 
 def reset_info(username):
+    """
+
+    Reset info about user's hobbies and preferred conversation topics
+    :param username: telegram username of a user
+    :type: string
+
+    """
     update_user_tb(username, "info_stage", 0)
     for hobby in Constants.hobbies_to_eng.values():
         update_user_hobbies_tb(username, hobby, 0)
@@ -119,6 +189,17 @@ def reset_info(username):
 
 
 def update_user_tb(username, feature, new_value):
+    """
+
+     Update a column in users table
+     :param username: telegram username of a user
+     :type: string
+     :param feature: column they want to change
+     :type: string
+     :param new_value: new value for that column
+     :type: string
+
+     """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -134,6 +215,17 @@ def update_user_tb(username, feature, new_value):
 
 
 def update_user_hobbies_tb(username, hobby, new_val=1):
+    """
+
+    Update information about user's hobbies
+    :param username: telegram username of a user
+    :type: string
+    :param hobby: type of hobby
+    :type: string
+    :type: int
+    :param new_val: value to update
+
+    """
     user_id = get_user_tb_column_val(username, "id")
     try:
         connection = create_connection()
@@ -150,6 +242,17 @@ def update_user_hobbies_tb(username, hobby, new_val=1):
 
 
 def update_user_topics_tb(username, topic, new_val=1):
+    """
+
+    Update information about user's preferred conversation topics
+    :param username: telegram username of a user
+    :type: string
+    :param topic: type of conversation topic
+    :type: string
+    :type: int
+    :param new_val: value to update
+
+    """
     user_id = get_user_tb_column_val(username, "id")
     try:
         connection = create_connection()
@@ -166,6 +269,16 @@ def update_user_topics_tb(username, topic, new_val=1):
 
 
 def check_existence(username):
+    """
+
+    Return information if an exact user exists in users table
+    :param username: telegram username of a user
+    :type: string
+
+    :rtype: bool
+    :return: True if user with such username exists, otherwise return False
+
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -181,6 +294,20 @@ def check_existence(username):
 
 
 def get_interest_val(username, column, hobbies=True):
+    """
+
+    Return if an exact user has an exact interest (hobby or preferred conversation topic)
+    :param username: telegram username of a user
+    :type: string
+    :param column: interest type
+    :type: string
+    :param hobbies: variable, which shows if we should check hobbies or preferred conversation topics
+    :type: string
+
+    :rtype: bool
+    :return: True if user with such username has such interest, otherwise return False
+
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -198,6 +325,16 @@ def get_interest_val(username, column, hobbies=True):
 
 
 def get_active_users(username):
+    """
+
+    Return all active users' usernames, which do not equal to an exact username
+    :param username: telegram username of a user we should ignore
+    :type: string
+
+    :rtype: list
+    :return: list of all active users ('is_active' = 1) with a different username
+
+    """
     try:
         connection = create_connection()
         cursor = connection.cursor()
